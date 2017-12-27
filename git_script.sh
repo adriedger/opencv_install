6deca93ddc64d32d020a4c49bd8f67cce103ff98
Andre Driedger <adriedge@gmail.com>
2017-12-26 23:31:16 -0700

    working

--- git_script.sh
+++ git_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ..
+cd ~
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
 git clone https://github.com/opencv/opencv_extra.git
